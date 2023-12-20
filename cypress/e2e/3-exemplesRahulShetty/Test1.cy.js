6b36fa8464fee5713d53ce975761ebbccd52980f
valeriano alexander <valerianoalexander@gmail.com>
2023-12-20 19:50:51 +0100

    20. Handling async promises with Cypress - Aliasing use AS and log and Console log difference.

--- cypress/e2e/3-exemplesRahulShetty/Test1.cy.js
+++ cypress/e2e/3-exemplesRahulShetty/Test1.cy.js
@@ -11,12 +11,18 @@ describe("My First Test Suite", () => {
     cy.get(".product").should("have.length", 5);
     cy.get(".product:visible").should("have.length", 4);
     // PArent child chaining
-    cy.get(".products").find(".product").should("have.length", 4);
+    cy.get(".products").as("productLocator");
+    cy.get("@productLocator").find(".product").should("have.length", 4);
     // Risk of break Path if we use xpath of an ellement in the DOM
     cy.get(":nth-child(4) > .product-action > button");
-    cy.get(".products").find(".product").eq(1).contains("ADD TO CART").click();
+    cy.get("@productLocator")
+      .find(".product")
+      .eq(1)
+      .contains("ADD TO CART")
+      .click()
+      .then(() => console.log("hello world"));
 
-    cy.get(".products")
+    cy.get("@productLocator")
       .find(".product")
       .each(($el, index, $list) => {
         const textVeg = $el.find("h4.product-name").text();
@@ -26,6 +32,9 @@ describe("My First Test Suite", () => {
           $el.find("button").trigger("cli ck");
         }
       });
+    // Assert if logo test is correctly displayed
+    cy.get(".brand").should("have.text", "GREENKART");
+    // This is to print in logs
     cy.get(".brand").then((logoElement) => {
       cy.log(logoElement.text());
     });
