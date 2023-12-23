4b42199266d1397eb312b4cd44cd084601aec2fc
valeriano alexander <valerianoalexander@gmail.com>
2023-12-23 19:37:26 +0100

    29. Handling web tables using each command

---
+++ cypress/e2e/3-exemplesRahulShetty/Test5.cy.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+describe("My Fifth Test Suite", () => {
+  it("My fifthTest case", () => {
+    // test step
+    cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+    cy.get(".table-display tr td:nth-child(2)").each(($el, i, $list) => {
+      let textCourse = $el.text();
+      if (textCourse.includes("Python")) {
+        cy.get(".table-display tr td:nth-child(2)")
+          .eq(i)
+          .next()
+          .then((price) => {
+            const priceText = price.text();
+            expect(priceText).to.equal("25");
+          });
+      }
+    });
+  });
+});
