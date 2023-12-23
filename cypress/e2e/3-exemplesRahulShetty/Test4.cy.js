d88865932923aeca97e2474d474e7c119846e43e
valeriano alexander <valerianoalexander@gmail.com>
2023-12-23 19:02:06 +0100

    28. NAvigating browser controls and Assertion

--- cypress/e2e/3-exemplesRahulShetty/Test4.cy.js
+++ cypress/e2e/3-exemplesRahulShetty/Test4.cy.js
@@ -3,9 +3,10 @@
 describe("My Fourth Test Suite", () => {
   it("My fourthTest case", () => {
     // test step
-    cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+    cy.visit("https://qaclickacademy.com/practice.php");
     cy.get("#alertbtn").click();
     cy.get("#confirmbtn").click();
+
     // windows :alert
     cy.on("window:alert", (str) => {
       //Mocha
@@ -19,5 +20,8 @@ describe("My Fourth Test Suite", () => {
     });
     // Using removeAttr-Jquery fonction for not flaky test and remove attribute target.
     cy.get("#opentab").invoke("removeAttr", "target").click();
+
+    cy.url().should("include", "qaclickacademy");
+    cy.go("back");
   });
 });
