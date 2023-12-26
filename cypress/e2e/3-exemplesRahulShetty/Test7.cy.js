4e5fcf45d8b2db495649af65b95c713c7e5421e7
valeriano alexander <valerianoalexander@gmail.com>
2023-12-26 18:48:50 +0100

    31. Handling child windows and tabs

---
+++ cypress/e2e/3-exemplesRahulShetty/Test7.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+describe("My Sixth Test Suite", () => {
+  it("My sixthTest case", () => {
+    // test step
+    cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+    cy.get("#opentab").then((el) => {
+      const url = el.prop("href");
+      cy.log(url);
+      cy.visit(url);
+    });
+  });
+});
