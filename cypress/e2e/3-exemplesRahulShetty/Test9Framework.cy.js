38a67b370a5bab416b7256794be2695245289807
valeriano alexander <valerianoalexander@gmail.com>
2023-12-28 17:41:04 +0100

    34. Test hooks avec DDT

--- cypress/e2e/3-exemplesRahulShetty/Test9Framework.cy.js
+++ cypress/e2e/3-exemplesRahulShetty/Test9Framework.cy.js
@@ -1,14 +1,15 @@
 /// <reference types="cypress" />
 
-describe("My Nineth Test Frames test", () => {
+describe("My Nine Hook Test Suite", () => {
   before(() => {
-    // root-level hook
-    // runs once before all tests
+    cy.log("*****This is a test before all *****");
   });
 
   it("My nineTest case Iframe", () => {
     cy.visit("https://rahulshettyacademy.com/angularpractice/");
-    cy.get("input[name='name']:nth-child(2)").type("bob");
-    cy.get("select").select("Female");
+    cy.fixture("exampleRahulShetty").then((data) => {
+      cy.get("input[name='name']:nth-child(2)").type(data.name);
+      cy.get("select").select(data.gender);
+    });
   });
 });
