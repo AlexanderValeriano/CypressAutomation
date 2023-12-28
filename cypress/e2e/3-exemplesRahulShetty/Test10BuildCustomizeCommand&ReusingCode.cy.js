d860c80e72f27f7abf2c88327ea02e8a45beec8f
valeriano alexander <valerianoalexander@gmail.com>
2023-12-28 19:39:27 +0100

    36. Before Building cypress commands for using code

---
+++ cypress/e2e/3-exemplesRahulShetty/Test10BuildCustomizeCommand&ReusingCode.cy.js
@@ -0,0 +1,34 @@
+/// <reference types="cypress" />
+
+describe("Validate attribute properties with Hook DDT Test Suite", () => {
+  let userdata;
+
+  before(() => {
+    cy.log("*****This is a test before all *****");
+
+    cy.fixture("exampleRahulShetty").then((data) => {
+      userdata = data;
+    });
+  });
+
+  it("ValidatingAttributePropertiesAndBehavior", () => {
+    cy.visit("https://rahulshettyacademy.com/angularpractice/");
+
+    cy.get("input[name='name']:nth-child(2)").type(userdata.name);
+    cy.get("select").select(userdata.gender);
+    cy.get(":nth-child(4) > .ng-pristine").should("have.value", userdata.name);
+    cy.get("input[name='name']:nth-child(2)").should(
+      "have.attr",
+      "minlength",
+      "2"
+    );
+
+    cy.get("#inlineRadio3").should("be.disabled");
+    cy.get(":nth-child(2) > .nav-link").click();
+    cy.get("h4.card-title").each(($el, i, $list) => {
+      if ($el.text().includes("Blackberry")) {
+        cy.get("button.btn.btn-info").eq(i).click();
+      }
+    });
+  });
+});
