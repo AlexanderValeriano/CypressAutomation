828235e9da72e60672aa40fb2d6ec6afdbac541a
valeriano alexander <valerianoalexander@gmail.com>
2023-12-28 17:46:25 +0100

    34. Fixtures and Data Driven Testing

--- cypress/e2e/3-exemplesRahulShetty/Test9Framework.cy.js
+++ cypress/e2e/3-exemplesRahulShetty/Test9Framework.cy.js
@@ -1,15 +1,27 @@
 /// <reference types="cypress" />
 
 describe("My Nine Hook Test Suite", () => {
+  let userdata;
+
   before(() => {
     cy.log("*****This is a test before all *****");
-  });
 
-  it("My nineTest case Iframe", () => {
-    cy.visit("https://rahulshettyacademy.com/angularpractice/");
     cy.fixture("exampleRahulShetty").then((data) => {
-      cy.get("input[name='name']:nth-child(2)").type(data.name);
-      cy.get("select").select(data.gender);
+      userdata = data;
     });
   });
+
+  it("FixtureDemoTest", () => {
+    cy.visit("https://rahulshettyacademy.com/angularpractice/");
+
+    cy.get("input[name='name']:nth-child(2)").type(userdata.name);
+    cy.get("select").select(userdata.gender);
+  });
+  // it("My nineTest case Iframe", () => {
+  //   cy.visit("https://rahulshettyacademy.com/angularpractice/");
+  //   cy.fixture("exampleRahulShetty").then((data) => {
+  //     cy.get("input[name='name']:nth-child(2)").type(data.name);
+  //     cy.get("select").select(data.gender);
+  //   });
+  // });
 });
